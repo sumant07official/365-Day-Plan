695ba983ce437a3c46917f433030a155feef9447
Sumant Jha <72351139+sumant07official@users.noreply.github.com>
2026-03-26 13:04:39 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'discipline-v1';
+const FILES = ['/365-discipline-tracker.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
